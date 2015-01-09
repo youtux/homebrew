1f91c5d24671c61f0ff29525429f0b6debddb5ee
Alessio Bogon <youtux@gmail.com>
2015-01-09 14:22:20 +0100

    wildfly: modernize

--- Library/Formula/wildfly-as.rb
+++ Library/Formula/wildfly-as.rb
@@ -1,13 +1,11 @@
-require 'formula'
-
 class WildflyAs < Formula
-  homepage 'http://wildfly.org/'
-  url 'http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz'
-  sha1 'd78a864386a9bc08812eed9781722e45812a7826'
+  homepage "http://wildfly.org/"
+  url "http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz"
+  sha1 "d78a864386a9bc08812eed9781722e45812a7826"
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
   end
 
   def caveats; <<-EOS.undent
